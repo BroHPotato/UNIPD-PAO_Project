1802427de7a55b4d7be7b659fb10c3cf8151dd8a
BroHPotato <giuseppevitobitetti@outlook.it>
2019-08-24 17:44:55 +0200

    Update genericfile.h

--- genericfile.h
+++ genericfile.h
@@ -12,7 +12,7 @@ public:
     virtual QString GetType() const;
     virtual int GetSizeData();
 private:
-    const QString typefile="File generico";
+    const QString typefile="File Generico";
 };
 
 
